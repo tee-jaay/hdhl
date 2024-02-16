6ff9f27f488aca62cc489318881ab0f5c88ceb0d
Tee Jaay <tamjid84@yahoo.com>
2024-02-16 12:35:31 +0600

    header, footer, sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/blog/header/Header";
+import Footer from "@/components/blog/footer/Footer";
+import Sidebar from "@/components/blog/sidebar/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-red-300">
+          <Header />
+        </div>
+        <div className="flex flex-row">
+          <main className="flex-grow bg-green-200">
+            {children}
+          </main>
+          <aside className="flex-none w-1/4 bg-yellow-300">
+            <Sidebar />
+          </aside>
+        </div>
+        <div className="bg-blue-300">
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import Header from "@/components/blog/header/Header";
-import Footer from "@/components/blog/footer/Footer";
-
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="">
-        <h1>Home page</h1>
-      </main>
-      <Footer />
-    </>
+    <h1>Home page</h1>
   );
 }
