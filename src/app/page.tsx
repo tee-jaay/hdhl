5d612fce433006a86e8c6b2181656644c4099e25
Tee Jaay <tamjid84@yahoo.com>
2024-02-17 21:37:34 +0600

    home refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
+import { League_Spartan } from "next/font/google";
+import Sidebar from "@/components/blog/sidebar/Sidebar";
+import Hero from "@/components/home/Hero";
+
+const leagueSpartan = League_Spartan({ subsets: ["latin"] });
+
 export default function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+      {/* Hero */}
+      <Hero fontClassName={leagueSpartan.className} />
+
+      <div className="carousel">
+        <div className="navigation">
+          <span>icon</span>
+          <span>icon</span>
+        </div>
+        <div className="items">
+          <div className="item">
+            <div className="item-image">image</div>
+            <div className="item-meta">
+              <h5 className="item-category">TRAVEL</h5>
+              <h4 className="item-title">Need some fresh air</h4>
+              <h6 className="item-date"><span className="">icon</span> April 20, 2022</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <aside className="flex-none w-1/4 bg-yellow-300">
+          <Sidebar />
+        </aside>
+      </div>
+    </>
   );
 }
