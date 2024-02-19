c5043b55e85ec8e4ea498b2205b5bb56764627b1
Tee Jaay <tamjid84@yahoo.com>
2024-02-19 15:14:27 +0600

    logo width to 100

--- src/components/blog/footer/FooterTop.tsx
+++ src/components/blog/footer/FooterTop.tsx
@@ -3,26 +3,35 @@ import SocialsLinksIcons from "@/components/common/SocialsLinksIcons";
 
 const FooterTop = () => {
     return (
-        <div className="text-white flex">
-            <div className="flex-1 logo_desc_follow_us">
-                <div className="flex flex-col">
-                    <div className="logo">
-                        <LogoWithLink />
+        <div className="flex">
+            <div>
+                <div className="text-white flex space-x-12">
+                    <div className="flex-1 logo_desc_follow_us">
+                        <div className="flex flex-col">
+                            <div className="logo">
+                                <LogoWithLink />
+                            </div>
+                            <div className="desc text-gray-200 my-8">
+                                Saepe possimus recusandae suscipit, consequatur, veritatis assumenda.
+                            </div>
+                            <h5 className="font-semibold text-gray-200 mb-2">Follow Us</h5>
+                            <div className="flex pt-3">
+                                <SocialsLinksIcons />
+                            </div>
+                        </div>
                     </div>
-                    <div className="desc text-gray-200 my-8">
-                        Saepe possimus recusandae suscipit, consequatur, veritatis assumenda.
+                    <div className="flex-1 tags_cloud">
+                        <h4 className="capitalize text-start">tags cloud</h4>
                     </div>
-                    <h5 className="font-semibold text-gray-200 mb-2">Follow Us</h5>
-                    <div className="flex pt-3">
-                        <SocialsLinksIcons />
+                    <div className="flex-1 quick_links">
+                        <h4 className="capitalize text-start">quick links</h4>
+                    </div>
+                    <div className="flex-1 newsletter_subscribe">
+                        <h4 className="capitalize text-start">newsletter</h4>
                     </div>
                 </div>
             </div>
-            <div className="flex-1 tags_cloud">
-                <h4 className="capitalize">tags cloud</h4>
-            </div>
-            <div className="flex-1 quick_links">quick links</div>
-            <div className="flex-1 newsletter_subscribe">newsletter</div>
+            <div></div>
         </div>
     );
 }
