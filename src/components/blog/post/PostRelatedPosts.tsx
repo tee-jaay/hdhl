dd10a9f083b24d78c362d01ffbd272461c77b9a6
Tee Jaay <tamjid84@yahoo.com>
2024-02-23 23:30:55 +0600

    related posts style fix

--- src/components/blog/post/PostRelatedPosts.tsx
+++ src/components/blog/post/PostRelatedPosts.tsx
@@ -46,15 +46,15 @@ interface Post {
 }
 
 const PostItem = ({ post }: { post: Post }) =>
-    <div className="post_item shadow-md">
-        <div className="post_image w-72">
+    <div className="post_item w-1/3 shadow-md">
+        <div className="post_image">
             <Image alt={post?.featuredImage?.node?.altText} src={post?.featuredImage?.node?.sourceUrl} width={288} height={250} />
         </div>
         <div className="post_data px-4 pb-4">
             <div className="post_date mt-7 text-[#999]">
                 {formatDate(post?.date, "numeric")}
             </div>
-            <div className="post_title mt-1">
+            <div className="post_title mt-1 line-clamp-2">
                 <Link href={post?.slug} title={post?.title}>
                     <div className="capitalize text-[#444] text-lg font-normal leading-tight hover:text-[#4ce5a2] transition ease-in-out duration-300">
                         {post?.title}
