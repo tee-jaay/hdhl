beb27b4511b60345de5607702acbffdf85d2e000
Tee Jaay <tamjid84@yahoo.com>
2024-02-24 20:30:13 +0600

    get in touch icons design

--- src/components/blog/sidebar/get-in-touch/GetInTouch.tsx
+++ src/components/blog/sidebar/get-in-touch/GetInTouch.tsx
@@ -1,14 +1,70 @@
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon, } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare, faInstagramSquare, faLinkedin, faVimeoSquare, faYoutubeSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons/faTwitterSquare";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
-const ConnectionItem = () => <Link href={"/"} className="connection_item flex space-x-2 shadow-lg p-1">
+const connectionItems = [
+    {
+        link: "/",
+        icon: faFacebookSquare,
+        color: "text-[#0866FF]",
+        text: "followers",
+        count: "5",
+    },
+    {
+        link: "/",
+        icon: faTwitterSquare,
+        color: "text-[#46C7FF]",
+        text: "fans",
+        count: "51",
+    },
+    {
+        link: "/",
+        icon: faInstagramSquare,
+        color: "text-[#FF3385]",
+        text: "likes",
+        count: "125",
+    },
+    {
+        link: "/",
+        icon: faVimeoSquare,
+        color: "text-[#87D4FF]",
+        text: "comments",
+        count: "5",
+    },
+    {
+        link: "/",
+        icon: faLinkedin,
+        color: "text-[#0E76A8]",
+        text: "followers",
+        count: "5",
+    },
+    {
+        link: "/",
+        icon: faYoutubeSquare,
+        color: "#EF242B",
+        text: "followers",
+        count: "467",
+    },
+];
+
+interface Item {
+    link: string,
+    icon: IconProp,
+    color: string,
+    text: string,
+    count: string,
+}
+
+
+const ConnectionItem = ({ item }: { item: Item }) => <Link href={"/"} className="connection_item flex space-x-2 shadow-lg p-1">
     <div className="conenction_icon">
-        <FontAwesomeIcon className="text-[#0866FF]" icon={faFacebookSquare} height={32} width={32} />
+        <FontAwesomeIcon className={item?.color} icon={item?.icon} height={32} width={32} />
     </div>
-    <div className="text_count text-[#000] text-xs">
-        <div className="capitalize">followers</div>
-        <div className="text-[#999]">750</div>
+    <div className="text_count text-[#222] text-xs">
+        <div className="capitalize">{item?.text}</div>
+        <div className="text-[#999]">{item?.count}</div>
     </div>
 </Link>
 
@@ -17,12 +73,7 @@ const GetInTouch = () => {
         <div className="mt-10">
             <h4 className="capitalize font-medium">get in touch</h4>
             <div className="socials_connections mt-3 grid grid-cols-2 gap-x-5 gap-y-4">
-                <ConnectionItem />
-                <ConnectionItem />
-                <ConnectionItem />
-                <ConnectionItem />
-                <ConnectionItem />
-                <ConnectionItem />
+                {connectionItems && connectionItems.map((item, i) => <ConnectionItem key={i} item={item} />)}
             </div>
         </div>
     );
