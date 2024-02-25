2e5f5d389f1e6be7c0a78caa0082e0e18cf12263
Tee Jaay <tamjid84@yahoo.com>
2024-02-25 15:40:40 +0600

    refactor

--- src/app/categories/[categorySlug]/[limit]/page.tsx
+++ src/app/categories/[categorySlug]/[limit]/page.tsx
@@ -54,10 +54,11 @@ const PostItem = ({ post }: { post: PostItemProps }) => <Link href={`/${post?.sl
                 <div className="date text-white">{formatDate(post?.date, "numeric")}</div>
                 <div className="author text-white flex line-clamp-1 items-center">
                     <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 mr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
                             <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
                         </svg>
-                    </span>{post?.author?.node?.name}
+                    </span>
+                    <span>{post?.author?.node?.name}</span>
                 </div>
             </div>
             <div className="post_title">
