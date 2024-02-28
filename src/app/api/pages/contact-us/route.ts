37d8221780bbbcc1ecbb2d682a012ea8e378e281
Tee Jaay <tamjid84@yahoo.com>
2024-02-28 08:52:59 +0600

    mail by mailgun

--- src/app/api/pages/contact-us/route.ts
+++ src/app/api/pages/contact-us/route.ts
@@ -1,3 +1,36 @@
-export async function GET(request: Request) {
-    return new Response('contact us');
+import Mailgun from "mailgun-js";
+
+const apiKey = process.env.MAILGUN_API_KEY || "";
+const domain = process.env.MAILGUN_DOMAIN || "";
+const contactEmail = process.env.CONTACT_EMAIL || "";
+
+const mailgun = new Mailgun({
+    apiKey: apiKey,
+    domain: domain,
+});
+
+interface MessageData {
+    from: string,
+    to: string,
+    subject: string,
+    text: string,
+}
+
+export async function POST(request: Request) {
+    const { formData } = await request.json();
+
+    const messageData: MessageData = {
+        from: formData.email,
+        subject: formData.subject,
+        text: formData.message,
+        to: contactEmail,
+    };
+
+    try {
+        await mailgun.messages().send(messageData);
+        return new Response("Message sent successfully");
+    } catch (error) {
+        console.error(error);
+        return new Response("Error sending message", { status: 500 });
+    }
 }
