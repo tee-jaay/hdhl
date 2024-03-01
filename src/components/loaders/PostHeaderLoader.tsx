9068a7c20e4de15dba77eaa399c48bda8a93dae4
Tee Jaay <tamjid84@yahoo.com>
2024-03-01 21:04:53 +0600

    loader width

--- src/components/loaders/PostHeaderLoader.tsx
+++ src/components/loaders/PostHeaderLoader.tsx
@@ -1,6 +1,6 @@
 const PostHeaderLoader = () => {
     return (
-        <div className="animate-pulse flex flex-col space-y-8 py-16 px-32">
+        <div className=" mx-auto animate-pulse flex flex-col space-y-8 py-16 px-32" style={{ width: "1024px" }}>
             <div className="bg-gray-200 w-full h-6"></div>
             <div className="flex space-x-4 justify-center">
                 <div className="bg-gray-200 w-1/5 h-3"></div>
