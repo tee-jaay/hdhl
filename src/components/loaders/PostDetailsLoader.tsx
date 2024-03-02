b4033e78f67f81b14a6a048d2523b91409386d2f
Tee Jaay <tamjid84@yahoo.com>
2024-03-02 09:30:34 +0600

    post header from ssr

---
+++ src/components/loaders/PostDetailsLoader.tsx
@@ -0,0 +1,28 @@
+const PostDetailsLoader = () => {
+    return (
+        <div className=" mx-auto animate-pulse flex flex-col space-y-8">
+            <div className="bg-gray-200 w-full h-[500px]"></div>
+            <div className="space-y-4">
+                <div className="bg-gray-200 h-3"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-3"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-2"></div>
+            </div>
+            <br />
+            <div className="space-y-4">
+                <div className="bg-gray-200 h-3"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-3"></div>
+                <div className="bg-gray-200 h-2"></div>
+                <div className="bg-gray-200 h-2"></div>
+            </div>
+        </div>
+    );
+}
+
+export default PostDetailsLoader;
