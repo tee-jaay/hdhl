4c253b84322281095b875010b99851f10b495cec
Tee Jaay <tamjid84@yahoo.com>
2024-03-04 00:49:56 +0600

    paginations styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import PostCardProps from "@/_models/PostCardProps";
+import formatDate from "@/_helpers/formatPostDate";
+import getLatestPosts from "@/_lib/graphQl/queries/getLatestPosts";
+import gqlQuery from "@/_lib/graphQl/gqlQuery";
 import AuthorAvatarNameLink from "@/components/common/AuthorAvatarNameLink";
 import CommentsCount from "@/components/common/CommentsCount";
 import PublishMonthDateYear from "@/components/common/PublishMonthDateYear";
-import getLatestPosts from "@/_lib/graphQl/queries/getLatestPosts";
-import gqlQuery from "@/_lib/graphQl/gqlQuery";
-import formatDate from "@/_helpers/formatPostDate";
-import PostCardProps from "@/_models/PostCardProps";
 
 const getData = async () => {
     // Construct the query and variables
     const query = getLatestPosts();
-    const variables = {
-        limit: 12,
-    };
+    const variables = { limit: 12, };
     try {
         // Make the request and return the data
         const data = await gqlQuery(query, variables);
@@ -41,7 +39,7 @@ const PostSingleCard = ({ post }: { post: PostCardProps }) => <div className="po
         </h1>
         <div className="post_excerpt text-[#777]" dangerouslySetInnerHTML={{ __html: post?.excerpt ?? "" }} />
         <div className="post_read_more mt-7">
-            <Link href={post?.slug} className="capitalize text-[#000] border border-[#999] py-2 px-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">read more</Link>
+            <Link href={post?.slug} className="capitalize text-[#000] border border-[#e2e2e2] py-2 px-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">read more</Link>
         </div>
     </div>
 </div>
@@ -49,9 +47,19 @@ const PostSingleCard = ({ post }: { post: PostCardProps }) => <div className="po
 const Blog = async () => {
     const posts = await getData();
     return (
-        <div className="posts_list space-y-9">
-            {posts && posts.map((post: PostCardProps, _id: number) => <PostSingleCard key={post.id} post={post} />)}
-            {posts.length < 1 && <p>No published post yet.</p>}
+        <div className="posts_container">
+            <div className="posts_list space-y-9">
+                {posts && posts.map((post: PostCardProps, _id: number) => <PostSingleCard key={post.id} post={post} />)}
+                {posts.length < 1 && <p>No published post yet.</p>}
+            </div>
+            <div className="paginations mt-8 flex space-x-2">
+                <div className="pagination_item text-[#444] cursor-pointer border font-medium text-sm flex items-center justify-center w-8 h-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">1</div>
+                <div className="pagination_item text-[#444] cursor-pointer border font-medium text-sm flex items-center justify-center w-8 h-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">2</div>
+                <div className="pagination_item text-[#444] cursor-pointer border font-medium text-sm flex items-center justify-center w-8 h-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">3</div>
+                <div className="pagination_item text-[#444] cursor-pointer border font-medium text-sm flex items-center justify-center w-8 h-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">4</div>
+                <div className="pagination_item text-[#444] cursor-pointer border font-medium text-sm flex items-center justify-center w-8 h-7 hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">5</div>
+                <div className="pagination_item text-[#444] cursor-pointer border font-medium text-sm flex items-center justify-center w-14 h-7 capitalize hover:text-[#4ce5a2] hover:border-[#4ce5a2] transition ease-in-out duration-300">next</div>
+            </div>
         </div>
     );
 }
