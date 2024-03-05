0082ca03999da543d02cd5be7e7004a28d1d08f4
Tee Jaay <tamjid84@yahoo.com>
2024-03-06 00:37:50 +0600

    static formdata

--- src/app/api/pages/contact-us/route.ts
+++ src/app/api/pages/contact-us/route.ts
@@ -1,7 +1,12 @@
 export async function POST(request: Request) {
     console.log(request.body);
     const mailerUrl = process.env.MAILER_URL;
-    const formData = {};
+    const formData = {
+        name: "Ali Bates",
+        email: "saradog@example.com",
+        subject: "Sint quis debitis fu",
+        message: "Quo omnis ut reprehe",
+    };
     try {
         // Make the request and return the data
         const res = await fetch(`${mailerUrl}/send-email`, {
@@ -9,7 +14,7 @@ export async function POST(request: Request) {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ formData }),
+            body: JSON.stringify(formData),
         });
         const data = await res.json();
         console.log('submitForm', data);
