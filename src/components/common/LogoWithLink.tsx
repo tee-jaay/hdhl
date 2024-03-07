4888395d2a0979c4ac7b707e931ee54aa070376d
Tee Jaay <tamjid84@yahoo.com>
2024-03-07 22:01:27 +0600

    logo to app name

--- src/components/common/LogoWithLink.tsx
+++ src/components/common/LogoWithLink.tsx
@@ -9,7 +9,7 @@ const LogoWithLink: React.FC = () => {
         <Link href={"/"} title={appName}>
             <div className="flex h-full items-center space-x-2">
                 <Image className="justify-center align-middle" src="/logo.png" alt="hdhl" width={100} height={60} style={{ width: "26px", height: "24px" }} />
-                <span className="font-extrabold text-2xl dark:text-white">H<span className="underline">D</span>HL</span>
+                <span className="font-extrabold text-2xl dark:text-white">{appName}</span>
             </div>
         </Link>
     );
