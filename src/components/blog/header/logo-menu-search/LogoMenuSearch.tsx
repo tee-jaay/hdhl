502600e34b3eec3160704df19622962265c84a21
Tee Jaay <tamjid84@yahoo.com>
2024-03-10 18:06:33 +0600

    small menu toggle show

--- src/components/blog/header/logo-menu-search/LogoMenuSearch.tsx
+++ src/components/blog/header/logo-menu-search/LogoMenuSearch.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Logo from "./logo/Logo";
 import MainMenu from "./main-menu/MainMenu";
 
-const LogoMenuSearch = () => {
+const LogoMenuSearch: React.FC = () => {
     return (
-        <div className="logo_menu_search md:w-[768px] lg:w-[1024px] mx-auto flex justify-between">
+        <div className="logo_menu_search md:w-[768px] lg:w-[1024px] mx-auto flex justify-between xs:py-6 xs:px-2 sm:px-0">
             <Logo />
             <MainMenu />
         </div>
