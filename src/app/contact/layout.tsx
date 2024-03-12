4ee74f64218de4d0ee92d35a6d673339e89f7eba
Tee Jaay <tamjid84@yahoo.com>
2024-03-12 15:00:34 +0600

    contact page dark color fix

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -7,8 +7,8 @@ export const metadata: Metadata = {
 
 export default function BlogLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <section className="flex flex-col mx-auto dark:text-white  dark:bg-[#333]">
-      <div className="blog_header w-full mx-auto py-16 bg-[#FBFAFA]">
+    <section className="flex flex-col mx-auto dark:text-white  dark:bg-[#222]">
+      <div className="w-full mx-auto py-16 bg-[#FBFAFA] dark:bg-[#333]">
         <h2 className="text-[#222] dark:text-white text-center laptop:text-2xl desktop:text-3xl font-medium tracking-wide">Contact Us</h2>
         <h6 className="text-[#8F8E8E] dark:text-white laptop:text- desktop:text-lg flex justify-center items-center">
           Home <span>
