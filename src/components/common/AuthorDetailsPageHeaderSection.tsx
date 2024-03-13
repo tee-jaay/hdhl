15680f3d8a192e3f2cdae8dd3d516ca5bf7b62de
Tee Jaay <tamjid84@yahoo.com>
2024-03-14 00:58:31 +0600

    profile pic in author detail

--- src/components/common/AuthorDetailsPageHeaderSection.tsx
+++ src/components/common/AuthorDetailsPageHeaderSection.tsx
@@ -4,15 +4,21 @@ import Link from "next/link";
 
 import gqlQuery from "@/_lib/graphQl/gqlQuery";
 import getUserBySlug from "@/_lib/graphQl/queries/getUserBySlug";
+import getUserProfile from "@/_lib/graphQl/queries/getUserProfile";
 
 const getData = async (params: string) => {
     // Construct the query and variables
     const query = getUserBySlug();
+    const profileQuery = getUserProfile();
     const variables = { id: params, };
     try {
         // Make the request and return the data
-        const data = await gqlQuery(query, variables);
-        return data?.user;
+        const userData = await gqlQuery(query, variables);
+        const profileData = await gqlQuery(profileQuery, variables);
+        return {
+            user: userData?.user,
+            profileImg: profileData?.guprofile,
+        };
     } catch (error) {
         // Handle the error here
         console.error(error);
@@ -21,7 +27,7 @@ const getData = async (params: string) => {
 }
 
 const AuthorDetailsPageHeaderSection: React.FC<{ slug: string }> = async ({ slug }) => {
-    const user = await getData(slug);
+    const { user, profileImg } = await getData(slug);
     return (
         <div className="blog_header py-16 bg-[#FBFAFA] dark:bg-[#333] w-full">
             <h2 className="tab:w-[640px] laptop:w-[768px] desktop:w-full mx-auto  text-[#222] dark:text-white text-center tab:text-2xl laptop:text-3xl desktop:text-4xl font-medium tracking-wide">{user?.name}</h2>
@@ -47,7 +53,7 @@ const AuthorDetailsPageHeaderSection: React.FC<{ slug: string }> = async ({ slug
                 </span>
             </h6>
             <div className="user_avatar mt-4 flex justify-center">
-                <Image alt="" src={user?.avatar?.url} width={100} height={100} />
+                <Image alt={profileImg?.featuredImage?.node?.altText} src={profileImg?.featuredImage?.node?.sourceUrl} width={100} height={100} />
             </div>
             <div className="tab:w-[640px] laptop:w-[768px] desktop:[1024px] mx-auto  mt-6 text-center dark:text-white text-[#555] phone:px-1 tab:px-2 laptop:px-4" dangerouslySetInnerHTML={{ __html: user?.description }} />
         </div>
