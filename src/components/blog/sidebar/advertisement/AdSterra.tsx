6eaa30d342f8cf6a73cc4fa292d02a01ad77a9b1
Tee Jaay <tamjid84@yahoo.com>
2024-04-04 03:17:24 +0600

    with suspense

--- src/components/blog/sidebar/advertisement/AdSterra.tsx
+++ src/components/blog/sidebar/advertisement/AdSterra.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 const AdsterraAd: React.FC = () => {
-  return <iframe
-    src="//www.topcreativeformat.com/1bc1b1376a6093cee5772c8748bfee12/invoke.js"
-    height="300"
-    width="160"
-  ></iframe>
+  return <Suspense fallback={<p>...</p>}>
+    <iframe
+      src="//www.topcreativeformat.com/1bc1b1376a6093cee5772c8748bfee12/invoke.js"
+      height="300"
+      width="160"
+    ></iframe>
+  </Suspense>
 };
 
 export default AdsterraAd;
--- src/components/blog/sidebar/advertisement/Advertisement.tsx
+++ src/components/blog/sidebar/advertisement/Advertisement.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 import AdsterraAd from "./AdSterra";
 
 const Advertisement: React.FC = () => {
-    // if (process.env.NODE_ENV === "production") {
-    //     return (
-    //         <div className="mt-12 h-[310px] border border-gray-400 flex py-8 justify-center items-center dark:text-[#FEFEFE]">
-    //             <AdsterraAd />
-    //         </div>
-    //     );
-    // } else {
-    return (
-        <div className="mt-12 border border-gray-400 flex py-8 justify-center items-center dark:text-[#FEFEFE]">
-            <Link href="/contact">Contact Us</Link>
-        </div>
-    );
-    // }
+    if (process.env.NODE_ENV === "production") {
+        return (
+            <div className="mt-12 h-[310px] border border-gray-400 flex py-8 justify-center items-center dark:text-[#FEFEFE]">
+                <AdsterraAd />
+            </div>
+        );
+    } else {
+        return (
+            <div className="mt-12 border border-gray-400 flex py-8 justify-center items-center dark:text-[#FEFEFE]">
+                <Link href="/contact">Contact Us</Link>
+            </div>
+        );
+    }
 }
 
 export default Advertisement;
