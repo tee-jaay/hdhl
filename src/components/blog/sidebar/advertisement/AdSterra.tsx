fcc2f93142b1e23455aec91c39262ab3d327d806
Tee Jaay <tamjid84@yahoo.com>
2024-04-03 07:24:26 +0600

    async

--- src/components/blog/sidebar/advertisement/AdSterra.tsx
+++ src/components/blog/sidebar/advertisement/AdSterra.tsx
@@ -3,6 +3,7 @@ import Script from "next/script";
 
 const AdsterraAd: React.FC = () => {
   return <Script
+    async
     strategy="lazyOnload"
     type="text/javascript"
     src="//www.topcreativeformat.com/1bc1b1376a6093cee5772c8748bfee12/invoke.js"
