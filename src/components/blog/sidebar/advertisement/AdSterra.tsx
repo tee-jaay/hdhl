88736ac58fb5625d3b773e06dda722453a3e66e1
Tee Jaay <tamjid84@yahoo.com>
2024-04-04 03:44:03 +0600

    adsterra with ref

--- src/components/blog/sidebar/advertisement/AdSterra.tsx
+++ src/components/blog/sidebar/advertisement/AdSterra.tsx
@@ -1,17 +1,42 @@
 "use client";
-import React, { Suspense } from "react";
 
-const AdsterraAd: React.FC = () => {
-  return <Suspense fallback={<p>...</p>}>
-    <iframe
-      src="//www.topcreativeformat.com/1bc1b1376a6093cee5772c8748bfee12/invoke.js"
-      width={160}
-      height={300}
-      style={{ border: 'none' }}
-      allowFullScreen
-      title="Ad Frame"
-    />
-  </Suspense>
+import React, { useEffect, useRef } from 'react';
+
+const AdSterra: React.FC = () => {
+  const bannerRef = useRef<HTMLDivElement>(null);
+
+  const adOptions = {
+    key: process.env.NEXT_PUBLIC_ADSTERRA_KEY, // Replace with your Adsterra key
+    format: 'iframe',
+    height: 250,
+    width: 320,
+    params: {},
+  };
+
+  useEffect(() => {
+    if (bannerRef.current && !bannerRef.current.firstChild) {
+      const confScript = document.createElement('script');
+      const adScript = document.createElement('script');
+
+      confScript.type = 'text/javascript';
+      confScript.innerHTML = `atOptions = ${JSON.stringify(adOptions)}`;
+
+      adScript.type = 'text/javascript';
+      adScript.src = `//www.topcreativeformat.com/${adOptions.key}/invoke.js`;
+
+      bannerRef.current.appendChild(confScript);
+      bannerRef.current.appendChild(adScript);
+    }
+  }, []);
+
+  return (
+    <div
+      className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center"
+      ref={bannerRef}
+    >
+      {/* Ads content will be loaded here */}
+    </div>
+  );
 };
 
-export default AdsterraAd;
+export default AdSterra;
