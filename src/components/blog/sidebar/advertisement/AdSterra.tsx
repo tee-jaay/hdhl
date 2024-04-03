e780d1e75f5931e3a69a39b90e18e8a4e834bd6d
Tee Jaay <tamjid84@yahoo.com>
2024-04-04 03:25:11 +0600

    iframe with suspense

--- src/components/blog/sidebar/advertisement/AdSterra.tsx
+++ src/components/blog/sidebar/advertisement/AdSterra.tsx
@@ -1,12 +1,16 @@
+"use client";
 import React, { Suspense } from "react";
 
 const AdsterraAd: React.FC = () => {
   return <Suspense fallback={<p>...</p>}>
     <iframe
       src="//www.topcreativeformat.com/1bc1b1376a6093cee5772c8748bfee12/invoke.js"
-      height="300"
-      width="160"
-    ></iframe>
+      width={160}
+      height={300}
+      style={{ border: 'none' }}
+      allowFullScreen
+      title="Ad Frame"
+    />
   </Suspense>
 };
 
