d3c8fb75b1ce44b51383eddabd5b9db93bf0cc90
Tee Jaay <tamjid84@yahoo.com>
2024-04-04 05:07:10 +0600

    empty value check

--- src/components/blog/sidebar/advertisement/Advertisement.tsx
+++ src/components/blog/sidebar/advertisement/Advertisement.tsx
@@ -8,7 +8,7 @@ const Advertisement: React.FC = () => {
     if (process.env.NODE_ENV === "production") {
         return (
             <div className="mt-12 h-[310px] border border-gray-400 flex py-8 justify-center items-center dark:text-[#FEFEFE]">
-                <AdSterra height={300} width={160} key={process.env.NEXT_PUBLIC_ADSTERRA_160_300_KEY || ""} />
+                <AdSterra height={300} width={160} key={process.env?.NEXT_PUBLIC_ADSTERRA_160_300_KEY || ""} />
             </div>
         );
     } else {
--- src/components/home/Advertisement.tsx
+++ src/components/home/Advertisement.tsx
@@ -6,10 +6,11 @@ import Link from "next/link";
 import AdSterra from "../common/advertisements/AdSterra";
 
 const Advertisement: React.FC<{}> = () => {
+
     if (process.env.NODE_ENV === "production") {
         return (
             <section className="laptop:w-[768px] desktop:w-[1024px] dark:text-white flex flex-col items-center justify-center mx-auto h-28 border border-black dark:border-white my-12 phone:px-2 tab:px-0">
-                <AdSterra height={90} width={728} key={process.env.NEXT_PUBLIC_ADSTERRA_728_90_KEY || ""} />
+                <AdSterra height={90} width={728} key={process.env?.NEXT_PUBLIC_ADSTERRA_728_90_KEY || ""} />
             </section>
         )
     } else {
