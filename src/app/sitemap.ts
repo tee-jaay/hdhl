bfa7f9373511819156c4a0122cc10b2bb6962a61
Tee Jaay <tamjid84@yahoo.com>
2024-06-17 23:57:38 +0600

    refactor dynamic sitemaps, fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,12 @@
 import { MetadataRoute } from "next";
+
 import { apiUrl, baseUrl } from "@/_lib/variables/constants";
+import getSitemapsArray from "@/_lib/helpers/getSitemapsArray";
 
 export async function generateSitemaps() {
     // Fetch and count total posts
-    const categoriesUrl = `${apiUrl}/wp-json/wp/v2/categories?_fields=id,count&per_page=100`;
-
-    try {
-        const response = await fetch(categoriesUrl);
-        if (!response.ok) {
-            throw new Error(`Error fetching categories: ${response.status}`);
-        }
-        const data = await response.json();
-        interface Category { id: number, count: number, }
-        // total posts
-        const totalPosts = data.reduce((acc: number, category: Category) => acc + category.count, 0);
-        // number of sitemap pages
-        const numSitemaps = Math.ceil(totalPosts / 100);
-        // sitemap objects
-        const sitemapsArr = Array.from({ length: numSitemaps }, (_, index) => ({ id: index, }));
-        return sitemapsArr;
-    } catch (error) {
-        console.error('Error generating sitemaps:', error);
-    }
+    const sitemapsArr = await getSitemapsArray();
+    return sitemapsArr;
 }
 
 interface MetadataRoute {
